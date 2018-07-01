5ce84c694f3c99117f19f3136f4b94eb74757b51
Daniel Patrick <dpatrick895@gmail.com>
2018-07-01 21:24:02 +1000

    created the database connection

--- twitterStreaming.py
+++ twitterStreaming.py
@@ -11,6 +11,9 @@ csecret = tt.getCsecret()
 atoken = tt.getAtoken()
 asecret = tt.getAsecret()
 
+def create_table():
+    c.execute("CREATE TABLE IF NOT EXISTS tweetsDB(username TEXT, tweet TEXT)")
+
 class listener(StreamListener):
     
     def on_data(self, data):
@@ -24,19 +27,30 @@ class listener(StreamListener):
             localtime = time.asctime( time.localtime(time.time()) )
             print(tweet)
             print(username)
-            #dynamic_data_entry(username, tweet)
+            dynamic_data_entry(username, tweet)
             
         
             return True
         except (BaseException, e):
             print("Failed ondata, ", str(e))
             
-            #c.close()
-            #conn.close()
+            c.close()
+            conn.close()
             time.sleep(5)
     
     def on_error(self, status):
         print(status)
+
+conn = sqlite3.connect('tweets.db')
+c = conn.cursor()
+create_table()
+
+def dynamic_data_entry(username, tweet):
+    
+    c.execute("INSERT INTO tweetsDB (username, tweet) VALUES (?,?)", 
+             (username, tweet))
+    
+    conn.commit()
     
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
